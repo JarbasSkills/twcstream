eac96582199e32972de6ffbd802314ac73197cba
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2021-03-11 11:17:12 -1000

    better_cps (#1)
    Co-authored-by: jarbasal <jarbasai@mailfence.com>

--- __init__.py
+++ __init__.py
@@ -1,53 +1,126 @@
-# -*- coding: utf-8 -*-
+from ovos_utils.skills.templates.common_play import BetterCommonPlaySkill
+from ovos_utils.playback import CPSMatchType, CPSPlayback, CPSMatchConfidence
+from os.path import join, dirname
 
-import time
-from mycroft.skills.core import MycroftSkill, intent_file_handler
-from mycroft.messagebus.message import Message
-from mycroft.util.log import LOG
 
-__author__ = 'aix'
-
-class TWCStream(MycroftSkill):
+class TWCStream(BetterCommonPlaySkill):
     def __init__(self):
         super(TWCStream, self).__init__(name="TWCStream")
+        self.url = "https://weather-lh.akamaihd.net/i/twc_1@92006/master.m3u8"
+        self.supported_media = [CPSMatchType.GENERIC,
+                                CPSMatchType.AUDIO,
+                                CPSMatchType.VIDEO,
+                                CPSMatchType.TV,
+                                CPSMatchType.NEWS]
+        self.default_image = join(dirname(__file__), "ui", "images",
+                                  "logo.png")
+        self.skill_logo = self.default_image
+        self.skill_icon = self.default_image
+        self.default_bg = join(dirname(__file__), "ui", "images",
+                               "background.png")
 
-    def initialize(self):
-        LOG.info("Initialized")
-        self.bus.on('twcstream.aiix.home', self.play_twc_live_stream)
+    # better_cps
+    def CPS_search(self, phrase, media_type):
+        """Analyze phrase to see if it is a play-able phrase with this skill.
 
-    # Create an intent for playing Sky News Live Stream
-    # Lets use a intent file to be able to call this function
-    
-    def launcher_id(self, message):
-        self.prepare_homepage({})
-        
-    def prepare_homepage(self, message):
-        self.gui.clear()
-        self.enclosure.display_manager.remove_active()
-        self.gui["loadingStatus"] = ""
-        self.gui.show_page("StreamLogo.qml", override_idle=True)
-        time.sleep(4)
-        self.gui.clear()
-        self.enclosure.display_manager.remove_active()
-        self.play_sky_live_stream({})
-    
-    @intent_file_handler("play_twc_live_stream.intent")
-    def play_twc_live_stream(self, message):
-        self.gui.clear()
-        self.enclosure.display_manager.remove_active()
-        # Get the URL of where the live stream is playing
-        set_url = "https://weather-lh.akamaihd.net/i/twc_1@92006/master.m3u8"
-        # Sending the stream to my GUI, creating a sessionData object
-        self.gui["videoState"] = "Play"
-        self.gui["videoStream"] = set_url
-        # This is the session data that is being sent to the qml page
-        # Asking the skill to display a page that will contain my video player
-        # Let's create the qml user interface for this
-        self.gui.show_page("TWCStream.qml", override_idle=True)
-    
-    @intent_file_handler("stop_twc_live_stream.intent")
-    def stop_twc_live_stream(self, message):
-        self.gui["videoState"] = "Stop"
-        
+        Arguments:
+            phrase (str): User phrase uttered after "Play", e.g. "some music"
+            media_type (CPSMatchType): requested CPSMatchType to search for
+
+        Returns:
+            search_results (list): list of dictionaries with result entries
+            {
+                "match_confidence": CPSMatchConfidence.HIGH,
+                "media_type":  CPSMatchType.MUSIC,
+                "uri": "https://audioservice.or.gui.will.play.this",
+                "playback": CPSPlayback.GUI,
+                "image": "http://optional.audioservice.jpg",
+                "bg_image": "http://optional.audioservice.background.jpg"
+            }
+        """
+        score = 0
+        if media_type == CPSMatchType.TV:
+            score += 30
+        if media_type == CPSMatchType.NEWS or self.voc_match(phrase, "news"):
+            score += 60
+        if self.voc_match(phrase, "weather"):
+            score += 40
+        if self.voc_match(phrase, "twc"):
+            score += 80
+        if score >= CPSMatchConfidence.AVERAGE:
+            if media_type not in [CPSMatchType.AUDIO, CPSMatchType.VIDEO,
+                                  CPSMatchType.TV]:
+                # audio + video results
+                results = [
+                    {
+                        # video match
+                        "match_confidence": min(100, score),
+                        "media_type": CPSMatchType.NEWS,
+                        "uri": self.url,
+                        "playback": CPSPlayback.GUI,
+                        "image": self.default_image,
+                        "bg_image": self.default_bg,
+                        "skill_icon": self.skill_icon,
+                        "skill_logo": self.skill_logo,
+                        "length": 0,
+                        "title": "The Weather Channel",
+                        "author": "The Weather Channel",
+                        "album": "The Weather Channel"
+                    },
+                    {  # audio match
+                        "match_confidence": min(100, score - 5),
+                        "media_type": CPSMatchType.NEWS,
+                        "uri": self.url,
+                        "playback": CPSPlayback.AUDIO,
+                        "image": self.default_image,
+                        "bg_image": self.default_bg,
+                        "skill_icon": self.skill_icon,
+                        "skill_logo": self.skill_logo,
+                        "length": 0,
+                        "title": "The Weather Channel (Audio)",
+                        "author": "The Weather Channel",
+                        "album": "The Weather Channel"
+                    }
+                ]
+            elif media_type == CPSMatchType.AUDIO:
+                # audio only results
+                results = [
+                    {
+                        # video match
+                        "match_confidence": min(100, score),
+                        "media_type": CPSMatchType.NEWS,
+                        "uri": self.url,
+                        "playback": CPSPlayback.AUDIO,
+                        "image": self.default_image,
+                        "bg_image": self.default_bg,
+                        "skill_icon": self.skill_icon,
+                        "skill_logo": self.skill_logo,
+                        "length": 0,
+                        "title": "The Weather Channel",
+                        "author": "The Weather Channel",
+                        "album": "The Weather Channel"
+                    }]
+            else:
+                # gui only results
+                results = [
+                    {
+                        # video match
+                        "match_confidence": min(100, score),
+                        "media_type": CPSMatchType.NEWS,
+                        "uri": self.url,
+                        "playback": CPSPlayback.GUI,
+                        "image": self.default_image,
+                        "bg_image": self.default_bg,
+                        "skill_icon": self.skill_icon,
+                        "skill_logo": self.skill_logo,
+                        "length": 0,
+                        "title": "The Weather Channel",
+                        "author": "The Weather Channel",
+                        "album": "The Weather Channel"
+                    }]
+            return results
+        return []
+
+
 def create_skill():
     return TWCStream()
